2f31a8f227abb1fc3a73e6898713d2d25913dee2
Mikko Jämiä <jamiamikko@Mikko-MacBook-Pro-5.local>
2017-01-24 13:03:19 +0200

    Start routerModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { RouterService } from './services/router.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavComponent } from './nav/nav.component';
@@ -19,9 +18,16 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: MainComponent
+      }
+    ])
   ],
-  providers: [RouterService],
+  providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
