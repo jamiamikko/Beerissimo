cb1ddc57d88228d531e8becc6d2ecd0c76d79830
odexi <otakar93@gmail.com>
2017-02-02 14:21:17 +0200

    Lataa nyt ja konflikti on valmis

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class LoginService {
 
-  constructor() { }
+  private url: String = 'http://media.mw.metropolia.fi/wbma';
+  private user: any = {};
+
+  constructor(private http: Http, private router: Router) { }
+
+   setUser = (user) => {
+    this.user = user;
+    console.log(this.user);
+  }
+
+  login = () => {
+    return this.http.post(this.url + '/login', this.user)
+     .subscribe(
+       resp => {
+         console.log(resp.json());
+         // convert user object to string and save userdata to local storage
+         // navigate to front
+        if (localStorage.getItem('user')){
+            this.router.navigate(['']);
+        }
+        else{
+          localStorage.setItem('user', JSON.stringify(this.user));
+          this.router.navigate(['']);
+        }
+       },
+       error => {
+         console.log(error);
+       }
+     );
+  }
 
 }
