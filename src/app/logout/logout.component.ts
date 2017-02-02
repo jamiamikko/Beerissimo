48a4216e17fd14d60d003a3d035fef0bae8a76c4
Mikko Jämiä <jamiamikko@Mikko-MacBook-Pro-5.local>
2017-02-02 14:48:54 +0200

    Services

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,4 +1,4 @@
-import { MediaService } from './../services/media.service';
+import { LogoutService } from './../services/logout.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor(private mediaService: MediaService) { }
+  constructor(private logoutService: LogoutService) { }
 
   ngOnInit() {
   }
 
 logout = () => {
-    this.mediaService.logout();
+    this.logoutService.logout();
   }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -8,6 +8,7 @@ export class LoginService {
 
   private url: String = 'http://media.mw.metropolia.fi/wbma';
   private user: any = {};
+  private loggedIn: boolean = false;
 
   constructor(private http: Http, private router: Router) { }
 
@@ -25,10 +26,12 @@ export class LoginService {
          this.user.token = dataFromServer.token;
 
         if (localStorage.getItem('user')){
+            this.loggedIn = true;
             this.router.navigate(['']);
         }
         else{
           localStorage.setItem('user', JSON.stringify(this.user));
+          this.loggedIn = true;
           this.router.navigate(['']);
         }
        },
